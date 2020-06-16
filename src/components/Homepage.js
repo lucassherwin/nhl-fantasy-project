9b5472150b8d87c31f0325dbe82898b2010eece3
lucassherwin <lksherwin@gmail.com>
2020-06-16 16:02:42 -0400

    search works

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -60,9 +60,6 @@ export class Homepage extends Component {
                         <p>Team stats here</p>
                     </div>
 
-                <label htmlFor='search'>Search By Player Name</label>
-                <input type='text' />
-                
                 {this.state.team !== null ? <PlayerTeam draftPlayers={this.draftPlayers} players={this.state.availablePlayers} getPlayers={this.getPlayers}
                 setAvailablePlayers={this.setAvailablePlayers} team={this.state.team}
                 setCurrentPlayer={this.props.setCurrentPlayer} userTeam={userTeam}
