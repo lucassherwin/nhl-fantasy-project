aa20f213163efffc5c54b66aeef366ededd86b24
lucassherwin <lksherwin@gmail.com>
2021-01-29 15:36:45 -0500

    made navbar functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav'
 
-export class Navbar extends Component {
-    render() {
-        return (
+export default function Navbar({ logOut }) {
+    return (
+        <div>
             <Nav variant="pills" defaultActiveKey="/homepage">
                 <Nav.Item>
                     <Nav.Link as={NavLink} to="/homepage" exact>Homepage</Nav.Link>
@@ -19,11 +19,9 @@ export class Navbar extends Component {
                     <Nav.Link as={NavLink} to="/createteam" exact>Create Team</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link as={NavLink} to="/login" exact onClick={this.props.logOut}>Log Out</Nav.Link>
+                    <Nav.Link as={NavLink} to="/login" exact onClick={logOut}>Log Out</Nav.Link>
                 </Nav.Item>
             </Nav>
-        )
-    }
+        </div>
+    )
 }
-
-export default Navbar
